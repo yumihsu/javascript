9563e4f28f67cf2b503276e6779a8212f0edce34
yumihsu <bappenlalay@gmail.com>
2021-10-15 02:09:48 +0800

    'language'

--- node_js/api/basic_server.js
+++ node_js/api/basic_server.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const fs = require("fs")
+const ip = "127.0.0.1";
+const port = 3000;
+
+
 //判斷網頁狀況  如果有readFile找不到的頁面會顯示error 500
 const sendResponse = (filename ,statusCode, response)=>{
     fs.readFile(`./html/${filename}`,(error,data)=>{
@@ -17,26 +21,30 @@ const sendResponse = (filename ,statusCode, response)=>{
 
 //server path
 const server = http.createServer((request,response) => {
-    console.log(request.url,":::",request.method);
-
     const method = request.method;
-    const url =  request.url;
-
+    let url =  request.url;
     if (method === "GET"){
-        if (url === "/"){
-            sendResponse("index.html",200, response);
-        }else if(url === "/about.html"){
-            sendResponse("about.html",200,response);
+        const requestUrl = new URL(url,`http://${ip}/${port}`);
+        let lang = requestUrl.searchParams.get('lang');
+        url = requestUrl.pathname;
+
+        if (lang===""|lang==="en"){
+            lang = ""
         }else{
-            sendResponse("404.html",404, response);
+            lang = `_${lang}`
         }
-    }else{
+
+        if (url === "/"){
+            sendResponse(`index${lang}.html`,200, response);
+        }else if(pathname === "/about.html"){
+            sendResponse(`about${lang}.html`,200,response);
+        }else{
+            sendResponse(`404${lang}.html`,404, response);
+        }
     }
-});
+})
 
 // server listing
-const ip = "127.0.0.1";
-const port = 3000;
 
 server.listen(port,ip, () =>{
     console.log(`server is running ${ip}:${port}`);
